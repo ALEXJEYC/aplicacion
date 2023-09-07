ad856d439245eee65fa5c63e1d1d56e68d8c8225
ALEXJEYC <101647682+ALEXJEYC@users.noreply.github.com>
2023-09-07 20:05:56 -0300

    actualizacion3
    se agrega css

--- myFirstApp/src/app/app-routing.module.ts
+++ myFirstApp/src/app/app-routing.module.ts
@@ -56,6 +56,8 @@ const routes: Routes = [
 
 
 
+
+
 ];
 
 @NgModule({
--- myFirstApp/src/environments/environment.prod.ts
+++ myFirstApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDFALWc8kV51YR1n3006-Kuq9lUrn-PCS4",
+    authDomain: "aplicacion-cata.firebaseapp.com",
+    projectId: "aplicacion-cata",
+    storageBucket: "aplicacion-cata.appspot.com",
+    messagingSenderId: "161940134110",
+    appId: "1:161940134110:web:b9725ba21ebe66e9c38a4f",
+   }
+
 };
